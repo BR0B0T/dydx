3f80517991972ba0eb18628fc6853e47418c2fec
br0b0t <dazzlerlogue@gmail.com>
2023-11-13 08:06:55 +0000

    connection to dydx

--- program/main.py
+++ program/main.py
@@ -1,6 +1,13 @@
-
+from func_connections import connect_dydx
 
 
 if __name__ == "__main__":
-    print('hello bot')
+    
+    # Connect to client
+    try:
+        print("Connecting to Client...")
+        client = connect_dydx()
+    except Exception as e:
+        print("Error connecting to client: ", e)
+        exit(1)
 
